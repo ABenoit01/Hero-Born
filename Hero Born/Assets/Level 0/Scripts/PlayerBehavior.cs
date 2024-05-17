5b0dfa3f0e7468ed56a9b293e7b35315f1143763
ABenoit01 <angelarmandobenoit@gmail.com>
2024-05-17 18:32:02 -0400

    Added jumping and moves some code arround in folders

--- Hero Born/Assets/Level 0/Scripts/PlayerBehavior.cs
+++ Hero Born/Assets/Level 0/Scripts/PlayerBehavior.cs
@@ -9,15 +9,24 @@ public class PlayerBehavior : MonoBehaviour
     private float _vInput;
     private float _hInput;
     // 1
+    public float DistanceToGround = 0.1f;
+    public float JumpVelocity = 5f;
+    private bool _isJumping;
+
     private Rigidbody _rb;
     // 2
+    public LayerMask GroundLayer;
+    private CapsuleCollider _col;
     void Start()
     {
         // 3
         _rb = GetComponent<Rigidbody>();
+
+        _col = GetComponent<CapsuleCollider>();
     }
     void Update()
     {
+        _isJumping |= Input.GetKeyDown(KeyCode.J);
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
         /*
@@ -33,11 +42,37 @@ public class PlayerBehavior : MonoBehaviour
         // 3
         Quaternion angleRot = Quaternion.Euler(rotation *
         Time.fixedDeltaTime);
+
+        //5
+        if (IsGrounded() && _isJumping)
+        {
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+
+        }
+
         // 4
         _rb.MovePosition(this.transform.position +
         this.transform.forward * _vInput * Time.fixedDeltaTime);
         // 5
+
+        
+
+
+        _isJumping = false;
+
         _rb.MoveRotation(_rb.rotation * angleRot);
     }
+
+    private bool IsGrounded()
+    {
+        // 7
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+
+        // 8
+        bool grounded = Physics.CheckCapsule(_col.bounds.center,capsuleBottom, DistanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
+
+        return grounded;
+    }
 }
+
 
